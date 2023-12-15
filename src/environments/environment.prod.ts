2266e91f00c7a4eb4340696da5862d4517b8cbe3
lyhork <lyhorklim@gmail.com>
2023-12-15 16:00:41 +0700

    main

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://127.0.0.1:9999/api',
+    fileUrl: 'http://127.0.0.1:8888/',
+    webUrl: 'http://127.0.0.1:7777/#/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    // apiUrl      : 'http://159.223.69.158:3901/api',
+    // fileUrl     : 'http://159.223.69.158:3904/',
+    // apiUrl      : 'http://159.223.69.158:3901/api',
+    // fileUrl     : 'http://159.223.69.158:3904/',
+    apiUrl: 'http://127.0.0.1:9999/api',
+    fileUrl: 'http://127.0.0.1:8888/',
+    webUrl: 'http://127.0.0.1:7777/#/',
+};
